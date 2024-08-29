c136b92ac3c069015309042f74fce49d6b471b2d
Luisa Tolentino <luisa.tolentino48@gmail.com>
2024-08-28 21:35:04 -0300

    Add files via upload

---
+++ ProgramacaoApp-main/Aula 15-08-2024/Desafios/Desafio1/desafio1.js
@@ -0,0 +1,89 @@
+// 1 - crie um programa que sorteei numeros da loteria, aleatoria, 
+//conforme o jogo escolhido. Não podem ter o mesmo numero, sem repeticao.
+
+//id jogo para acrescentar os jogos criados
+let jogo = document.getElementById("jogo");
+
+//Limpa o html
+function limpar(){
+    jogo.innerHTML = "";
+}
+
+function escolherLoteria(){
+    //alert("Entrei!");
+    var opcoes = document.getElementById("opcoesLoteria");
+    //O texto do jogo selecionado (mega sena; quina; lotofacil)
+    var text = opcoes.options[opcoes.selectedIndex].text;
+    let listaJogo = [];
+    if(text != null && text != ""){
+        switch (text) {
+            case "Mega Sena":{
+                // //console.log("Entrei")
+                let count = 0;
+                let maxNumbers = 6;
+                
+                while(count < maxNumbers){
+                    let numberRandom = Math.floor(Math.random() * 100);;
+                    // console.log(numberRandom);
+                    // console.log(listaJogo.indexOf(numberRandom));
+                    if(listaJogo.indexOf(numberRandom) == -1 && numberRandom > 0 && numberRandom < 61){
+                        // console.log("entrei");
+                        listaJogo.push(numberRandom);
+                        count++;
+                    }
+                    // console.log(count);
+                }
+                break;
+            }
+            case "Quina":{
+                let count = 0;
+                let maxNumbers = 5;
+                
+                while(count < maxNumbers){
+                    let numberRandom = Math.floor(Math.random() * 100);;
+                    // console.log(numberRandom);
+                    // console.log(listaJogo.indexOf(numberRandom));
+                    if(listaJogo.indexOf(numberRandom) == -1 && numberRandom > 0 && numberRandom < 81){
+                        // console.log("entrei");
+                        listaJogo.push(numberRandom);
+                        count++;
+                    }
+                    // console.log(count);
+                }
+                break;
+            }   
+            case "Lotofácil":{
+                // console.log("Entrei")
+                let count = 0;
+                let maxNumbers = 15;
+                
+                while(count < maxNumbers){
+                    let numberRandom = Math.floor(Math.random() * 100);;
+                    // console.log(numberRandom);
+                    // console.log(listaJogo.indexOf(numberRandom));
+                    if(listaJogo.indexOf(numberRandom) == -1 && numberRandom > 0 && numberRandom < 26){
+                        // console.log("entrei");
+                        listaJogo.push(numberRandom);
+                        count++;
+                    }
+                    // console.log(count);
+                }
+                break;
+            }    
+            default:
+                alert("Opção invalida");
+                break;
+        }
+        let h1 = document.createElement("h1");
+        h1.innerText = text;
+        jogo.appendChild(h1);
+        let concatNumeros = "";
+        let p = document.createElement("p");
+        listaJogo.forEach(item => {
+            //document.write();
+            p.innerText += `${item} - `;
+        });
+        // console.log(concatNumeros);
+        jogo.appendChild(p);
+    }
+}
---
+++ ProgramacaoApp-main/Aula 15-08-2024/script.js
@@ -0,0 +1,33 @@
+//Definição de lista
+let lista = ["Victor", "Sabrina", "Maria", "Olivia", 150];
+
+//Percorre todos os elementos
+lista.forEach(item => {
+    console.log(item);
+}); 
+//Encontrar o index de um elemento
+let indexOf150 = lista.indexOf("Olivia");
+console.log("\n");
+
+lista[indexOf150] = "Olivia Rodrigo";
+//Elimina o ultimo elemento
+lista.pop();
+//Adiciona um novo elemento ao inicio da lista
+lista.unshift("Antonia");
+//Elimina o primeiro elemento de uma lista
+lista.shift();
+lista.forEach(item => {
+    console.log(item);
+}); 
+
+
+//Altera o sepador da lista de ',' para '/'
+console.log(lista.join("/"));
+
+let lista2 = ["Fruta", "Verdura", "Carne", "Besteira"];
+//Une um array em outro
+let listaConcatLista2 = lista.concat(lista2);
+
+listaConcatLista2.forEach(item => {
+    console.log(item);
+}); 
